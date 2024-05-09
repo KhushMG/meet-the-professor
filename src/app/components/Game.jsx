abd7cba822bb8127333426f5574eed7da73b58b4
KhushMG <krg04@uw.edu>
2024-05-08 20:11:14 -0700

    welcome and bg

--- src/app/components/Game.jsx
+++ src/app/components/Game.jsx
@@ -2,13 +2,14 @@ import Background from "../assets/background.jpg";
 import Image from "next/image"
 export default function Game() {
   return (
-
+    <> 
       <Image
-        src={Background}
-        fill
-        alt="background"
-        className="min-h-screen overflow-hidden"
-      />
-
+          src={Background}
+          fill
+          alt="background"
+          className="min-h-screen overflow-hidden"
+        />
+    </>
+      
   );
 }
--- src/app/components/Welcome.jsx
+++ src/app/components/Welcome.jsx
@@ -9,21 +9,31 @@ import {
   Dialog,
 } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function Welcome() {
 
   const [isOpen, setIsOpen] = useState(true); // Control the open state
   const handleClose = () => setIsOpen(false); // Handler to close the dialog
 
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
   return (
     <Dialog defaultOpen open={isOpen} onOpenChange={setIsOpen}>
       <DialogTrigger asChild>
         <button className="hidden" /> 
       </DialogTrigger>
-      <DialogContent className="sm:max-w-[425px] bg-white rounded-lg shadow-lg p-5">
+      <DialogContent className="sm:max-w-[40rem] bg-white rounded-lg shadow-lg p-5">
         <DialogHeader>
-          <DialogTitle>Welcome to our app!</DialogTitle>
+          <DialogTitle>Welcome to Prof-iler!</DialogTitle>
           <DialogDescription>
             We&apos;re excited to have you on board. Let&apos;s get started.
           </DialogDescription>
